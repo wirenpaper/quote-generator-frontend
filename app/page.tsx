178453adfde1dacad51d64c6f23cb559a10f77cc
wnp <chumchumchira@gmail.com>
2024-06-24 11:10:25 +0500

    useEffect fetches quotes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+
+interface Quote {
+  text: string;
+  author?: string;
+}
+
+export default function Quotes() {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+
+  useEffect(() => {
+    async function fetchQuotes() {
+      const res = await fetch("https://type.fit/api/quotes");
+      const data: Quote[] = await res.json();
+      setQuotes(data);
+    }
+    fetchQuotes();
+  }, []);
+
   return (
-  <div className="p-14"></div>
+    <div>
+      <h1>Quotes</h1>
+      <ul>
+        {quotes.slice(0, 15).map((quote, index) => (
+          <li key={index}>
+            "{quote.text}" - {quote.author || "Unknown"}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
